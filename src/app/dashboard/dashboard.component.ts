36f8cc66b67293fa4062d76af7522d7d6f191f8e
ETorresSacha <eriktorressacha@gmail.com>
2023-09-28 02:51:52 -0500

    logout

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
+  constructor( 
+    private router:Router
+    ) { }
+
+  logout(){
+    console.log("salir");
+    localStorage.clear()
+    this.router.navigate(['login'])
+    
+  }
 
 }
